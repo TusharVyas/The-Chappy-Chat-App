bf1e3209dac65937c53460ba505f037ee70222b6
Siddhartha Vyas <siddharthavyas67@gmail.com>
2022-09-25 23:11:41 +0530

    initial commit

---
+++ public/src/components/Messages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+function Messages() {
+  return <Container>Messages</Container>;
+}
+
+export default Messages;
+const Container = styled.div`
+  height: 80%;
+`;
